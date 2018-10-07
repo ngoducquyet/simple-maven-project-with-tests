9089761cd2c32bdf2741216153aa799fd3f06813
JourneyVN <ngoducquyet@yahoo.com>
2018-10-07 12:29:20 +0700

    Update SomeTest.java

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -31,21 +31,16 @@ public class SomeTest extends Base {
     @Test public void test1() {
         run();
     }
-
     @Test public void test2() {
         run();
     }
-
     @Test public void test3() {
         run();
     }
-
     @Test public void test4() {
         run();
     }
-
     @Test public void test5() {
         run();
     }
-
 }
